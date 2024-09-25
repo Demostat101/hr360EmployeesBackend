a32a22be02fe2653cc7b5f88d495b8b90a4b0558
Demostat101 <demostat1995@gmail.com>
2024-09-25 18:00:36 +0100

    add cors to specified site

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ const db = require("./db");
 const routes = require("./routes");
 require("dotenv").config();
 const cors = require("cors");
-
 const PORT = process.env.PORT || 3500;
-
 const app = express();
 
-app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://hr360dashboard-omikunle-ademola.netlify.app'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+};
+
+app.use(cors(corsOptions));
+
+// app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(bodyParser.json());
